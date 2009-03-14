a9045eee9d8ec81e12bb9819df82e45575f6427f
Emmanuele Bassi <ebassi@gnome.org>
2009-03-14 23:24:24 +0000

    Build GObject Introspection data
    Twitter-GLib should provide the Introspection data (GIR and typelib)
    that is now becoming part of the required featureset of a GNOME
    library.

--- autogen.sh
+++ autogen.sh
@@ -20,7 +20,21 @@ else
         gtkdocize || exit $?
 fi
 
-autoreconf -v --install || exit $?
+GLIB_GETTEXTIZE=`which glib-gettextize`
+if test -z $GLIB_GETTEXTIZE; then
+	echo "*** No glib-gettextize ***"
+	exit 1
+else
+	glib-gettextize -f || exit $?
+fi
+
+AUTORECONF=`which autoreconf`
+if test -z $AUTORECONF; then
+	echo "*** No autoreconf found ***"
+	exit 1
+else
+	ACLOCAL="${ACLOCAL-aclocal} $ACLOCAL_FLAGS" autoreconf -v --install || exit $?
+fi
 
 ./configure "$@" && echo "Now type 'make' to compile ${PROJECT}"
 
